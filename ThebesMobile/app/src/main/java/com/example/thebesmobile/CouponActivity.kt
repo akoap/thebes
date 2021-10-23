d0aa0f7689fc120651d25afd3c8f75f0c68a5e36
Akshay Patil <avpat02@gmail.com>
2021-10-23 14:59:52 -0400

    fixed xml not loading issue

--- ThebesMobile/app/src/main/java/com/example/thebesmobile/CouponActivity.kt
+++ ThebesMobile/app/src/main/java/com/example/thebesmobile/CouponActivity.kt
@@ -1,7 +1,12 @@
 package com.example.thebesmobile
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class CouponActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coupon)
+    }
 
 }
